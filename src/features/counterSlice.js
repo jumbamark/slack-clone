a11352a1807a2f9f9bf1806cc6cc1cb2b9b2aed1
Jumba Mark <jumbamark@yahoo.com>
2022-01-21 23:50:33 +0300

    first commit

--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -1,9 +1,8 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
   value: 0,
-  status: 'idle',
+  status: "idle",
 };
 
 // The function below is called a thunk and allows us to perform async logic. It
@@ -11,17 +10,14 @@ const initialState = {
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched. Thunks are
 // typically used to make async requests.
-export const incrementAsync = createAsyncThunk(
-  'counter/fetchCount',
-  async (amount) => {
-    const response = await fetchCount(amount);
-    // The value we return becomes the `fulfilled` action payload
-    return response.data;
-  }
-);
+export const incrementAsync = createAsyncThunk("counter/fetchCount", async (amount) => {
+  // const response = await fetchCount(amount);
+  // The value we return becomes the `fulfilled` action payload
+  // return response.data;
+});
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
@@ -45,16 +41,16 @@ export const counterSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(incrementAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(incrementAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
+        state.status = "idle";
         state.value += action.payload;
       });
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const {increment, decrement, incrementByAmount} = counterSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// import {initializeApp} from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9L8EQlKJBpuNbn9rXVbVOz0hOPEx5IqU",
+  authDomain: "slack-clone-b512e.firebaseapp.com",
+  projectId: "slack-clone-b512e",
+  storageBucket: "slack-clone-b512e.appspot.com",
+  messagingSenderId: "216744178275",
+  appId: "1:216744178275:web:1911dbc7d5d400e3ffbe56",
+};
+
+// Initialize Firebase
