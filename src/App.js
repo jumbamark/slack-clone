b57c11bd2322ad2225e653c01cbc4fbe946918fd
Jumba Mark <jumbamark@yahoo.com>
2022-01-22 16:19:38 +0300

    set up react routing and added the header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build slack</h1>
+      <BrowserRouter>
+        <>
+          <Routes>
+            <Route path="/" exact element={<Header />} />
+            {/* <Route path="users/*" element={<Users />} /> */}
+          </Routes>
+        </>
+      </BrowserRouter>
     </div>
   );
 }
