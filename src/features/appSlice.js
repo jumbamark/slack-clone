3de8cba9a8bc7f27d0c8b7eb7398a7ba464c2857
Jumba Mark <jumbamark@yahoo.com>
2022-01-24 18:26:13 +0300

    added redux to the project

---
+++ src/features/appSlice.js
@@ -0,0 +1,26 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+  roomId: null,
+};
+
+export const appSlice = createSlice({
+  name: "app",
+  initialState,
+
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    enterRoom: (state, action) => {
+      state.roomId = action.payload.roomId;
+    },
+  },
+});
+
+export const {enterRoom} = appSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const selectRoomId = (state) => state.app.roomId;
+
+export default appSlice.reducer;
