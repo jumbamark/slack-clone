8b770380a6c306f3de3989cc74bbef5e927b36a0
Jumba Mark <jumbamark@yahoo.com>
2022-01-27 13:42:45 +0300

    added jsx that shows when the app is loading

--- src/App.js
+++ src/App.js
@@ -8,9 +8,21 @@ import Chat from "./components/Chat";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "././firebase";
 import Login from "./components/Login";
+import Spinner from "react-spinkit";
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" alt="" />
+          <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
 
   return (
     <div className="app">
@@ -41,4 +53,26 @@ export default App;
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
+`;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
 `;
